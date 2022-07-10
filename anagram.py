240e0b50048aeb715f2dc46adc14d8df52dadaa8
Kendra <kendra@Zipcoders-MacBook-Pro-45.local>
2022-07-10 14:43:03 -0400

    finished anagram problem

--- anagram.py
+++ anagram.py
@@ -2,5 +2,5 @@ def is_anagram(first_string: str, second_string: str) -> bool:
     """
     Given two strings, this functions determines if they are an anagram of one another.
     """
-    return first_string.contains(second_string)# remove pass statement and implement me
+    return sorted(first_string) == sorted(second_string)# remove pass statement and implement me
 
